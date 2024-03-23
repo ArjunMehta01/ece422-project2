8e5480f1b875adb27733ea2c1b94264a27fff6a0
intelliwavetech\svirk <svirk@intelliwavetechnologies.com>
2024-03-22 21:20:09 -0600

    push to pc

--- server/src/auth.py
+++ server/src/auth.py
@@ -3,5 +3,25 @@
 # creating groups
 # modifying users and adding them to groups
 
+from filesystem import getUsers
+from argon2 import PasswordHasher
+
 class user:
 	pass
+
+def login(login_string):
+	username, password, pub_key = login_string.split(' ')
+
+	user_dict = getUsers()
+	if username not in user_dict:
+		return (False, None)
+	else:
+		user = user_dict[username]
+		acc_pass_hash = user.getPassHash()
+		salt = user.getPassSalt()
+		test_pass_hash = PasswordHasher.hash(password, salt)
+
+		if acc_pass_hash == test_pass_hash:
+			return (True, user)
+		else:
+			return (False, None)
