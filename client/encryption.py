3258d1437f8c3beae4c9f480436a613fbc7449e8
intelliwavetech\svirk <svirk@intelliwavetechnologies.com>
2024-03-26 14:27:26 -0600

    poopity scoop

--- client/encryption.py
+++ client/encryption.py
@@ -2,10 +2,6 @@ import rsa
 
 MESSAGE_SIZE = 1024
 
-# takes a raw socket and gives sendall + recv methods that do communication with rsa encryption
-# load up the private key from a local file
-# include a method to set the client pubkey after construction since it only comes in on the first decrypted message
-# send recv methods should include a standard buff size so we dont need to specify it - shit design but fuck it we go fast
 class rsaSocket:
 	def __init__(self, connection):
 		self.connection = connection
@@ -30,11 +26,9 @@ class rsaSocket:
 		encrypted = self.connection.recv(MESSAGE_SIZE)
 		message = rsa.decrypt(encrypted, self.priv_key).decode()
 		return message
-
-
+	
 def loadPrivKey():
-	with open('../.secrets/id_rsa', 'r') as priv_key_file:
+	with open('./.secrets/id_rsa', 'r') as priv_key_file:
 		private_key_string = priv_key_file.read().strip()
 		private_key = rsa.PrivateKey.load_pkcs1(private_key_string)
 	return private_key
-		
