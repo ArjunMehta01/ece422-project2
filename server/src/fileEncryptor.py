2502f3cb597e71664700a06ed8aff9b0037c678b
virkSehbazz12 <71534099+virkSehbazz12@users.noreply.github.com>
2024-04-07 13:20:32 -0600

    Revert "Merge pull request #14 from ArjunMehta01/yimeng"
    This reverts commit d7b1fb776fcd9cd85be78a30f857e0f2f16ce443, reversing
    changes made to 69ec4d8188b976cf4f4926b6012457b95fda8947.

--- server/src/fileEncryptor.py
+++ server/src/fileEncryptor.py
@@ -42,14 +42,12 @@ def storeFile(encFilepath: str, filename: str, content: str, encryptFileName = T
         encFileName = filename
     
     signFileName = encFileName + '.sign'
-    
-    FILE_SYSTEM_PATH = os.getenv('FILESYSTEM_PATH')
-    # print(FILE_SYSTEM_PATH)
-    newFileFullPath = FILE_SYSTEM_PATH + encFilepath + '/' + encFileName
-    newSignFullPath = FILE_SYSTEM_PATH + encFilepath + '/' + signFileName
+
+    newFileFullPath = FILE_SYSTEM_PATH + encFilepath + '\\' + encFileName
+    newSignFullPath = FILE_SYSTEM_PATH + encFilepath + '\\' + signFileName
 
     encContent = fernet.encrypt(content.encode())
-
+    
     hasher.update(content.encode())
     hashedContent = hasher.digest()
     signature = fernet.encrypt(hashedContent)
@@ -65,7 +63,6 @@ def storeFile(encFilepath: str, filename: str, content: str, encryptFileName = T
 
 def getFile(filename):
     """Given an encrypted filename, returns the decrypted content of the file."""
-    FILE_SYSTEM_PATH = os.getenv('FILESYSTEM_PATH')
     signatureFileName = filename + '.sign'
     fernet = Fernet(FERNET_KEY)
     hasher = hashlib.sha256()
